a61a218c0371caa1104b7819bbe729002dbda373
life-0 <1471925853@qq.com>
2021-05-14 09:24:06 +0800

    commit_2

--- mp3.py
+++ mp3.py
@@ -49,23 +49,35 @@ def findAndInput(path, matchedMusicPath):  # 相对路径
 # musicName : 歌单的一行数据 歌名 - 作者名
 # matchedMusic_list : 被匹配的数据集合 歌名 - 作者=路径=文件大小
 def adaptation(musicName, matchedMusic_list):
-    matchName = re.split(r'[\s.、()（）&:_\\/-]',  # "".join(musicName.split()).lower()
-                         musicName.lower())  # 一丝不挂 - 陈奕迅 分割文本 好进行匹配
-    print(musicName)
-    while "" in matchName:  # 去除列表中的空字符串
-        matchName.remove("")
+    musicName = re.sub(u"\\(.*?\\)|\\(.*?）|\\（.*?\\)|\\{.*?}|\\[.*?]|（.*?）|《.*?》|\s|/|\.|&|\\\|:|_", "",
+                       musicName)
+    match_music = musicName[:musicName.find('-')]  # 从左往右 只要到第一个"-" 视为歌名
+    match_author = musicName[musicName.rfind('-'):]  # 从右往左 只要到第一个"-" 视为作者
+    match_author_music = match_music + match_author  # 匹配值: 歌名-作者
+    # matchName = re.split(r'[\s.、()（）&:_\\/-]',  # "".join(musicName.split()).lower()
+    #                      musicName.lower())  # 一丝不挂 - 陈奕迅 分割文本 好进行匹配
+    # print(musicName)
+    # while "" in matchName:  # 去除列表中的空字符串
+    #     matchName.remove("")
 
     matchFlag = 0  # 设置匹配度 歌名与歌单=路径搜索结果的匹配程度
     for matchedKey, matchedValue in matchedMusic_list.items():
         # print(musicName.lower(),matchedValue[0].lower())
-        matchValue = difflib.SequenceMatcher(None, musicName.lower(), matchedValue[0].lower()).quick_ratio()
+        matchedValue = re.sub(u"\\(.*?\\)|\\(.*?）|\\（.*?\\)|\\{.*?}|\\[.*?]|（.*?）|《.*?》|\s|/|\.|&|\\\|:|_", "",
+                              matchedValue[0])
+        matched_music = matchedValue[:matchedValue.find('-')]  # 从左往右 只要到第一个"-" 视为歌名
+        matched_author = matchedValue[matchedValue.rfind('-'):]  # 从右往左 只要到第一个"-" 视为作者
+        matched_author_music = matched_music + matched_author  # 匹配值: 歌名-作者
+
+        matchValue = difflib.SequenceMatcher(None, match_author_music.lower(),
+                                             matched_author_music.lower()).quick_ratio()  # matchedValue[0].lower()
         if matchValue > 0.8:  # 匹配度超过80% 视为查找到数据
             # fileSize = os.path.getsize(targetAtr[-1])  # 匹配度文件查找
             matchFlag += 1
-            print("匹配度:", matchValue, musicName, matchedValue[0], matchedValue)
+            print("匹配度:", matchValue, musicName, ":", matchedValue)
             break
-        if matchedKey == len(matchedMusic_list):
-            print("")
+        # if 0.5 < matchValue <= 0.79:
+        #     print("低匹配度:", musicName, ":",matched_music.lower())
     if matchFlag == 0:
         failedMusicSearch(musicName, matchedMusic_list)
         # for matchedKey, matchedValue in matchedMusic_list.items():
@@ -94,29 +106,37 @@ def adaptation(musicName, matchedMusic_list):
     #     failedMatch.append(musicName)
 
 
-# failedMatch_list: 失败匹配的音乐集合
-# matchedMusic_list:被匹配的数据集合 歌名 - 作者=路径=文件大小
+# failedMatch_list: 失败匹配的音乐集合 歌名 - 作者
+# matchedMusic_list:被匹配的数据集合 [歌名 - 作者,路径,文件大小]
 def failedMusicSearch(failedMatch_list, matchedMusic_list):
     # for music in failedMatch_list:
-    matchName = re.split(r'[\s.、()（）&:_\\/-]',  # "".join(musicName.split()).lower()
-                         failedMatch_list.lower())  # 一丝不挂 - 陈奕迅 分割文本 好进行匹配
-    while "" in matchName:  # 去除列表中的空字符串
-        matchName.remove("")
-    for matchedMusicKey, matchedMusicValue in matchedMusic_list.items():
-        matched = re.split(r'[\s.、()（）&:_\\/-]',  # "".join(musicName.split()).lower()
-                           matchedMusicValue[0].lower())
-        while "" in matched:  # 去除列表中的空字符串
-            matched.remove("")
-        matchDegree = len(matchName) if len(matchName) <= len(matched) else len(matched)
-        check_list = matchName if len(matchName) <= len(matched) else matched
-        for i in len(check_list):
+    match_authors = re.split(r'[-]', failedMatch_list.lower())  # 目的: 分割出作者名
+    match_authors = re.split(r'[\s.、()（）&:_\\/-]', match_authors[-1])  # 细化多个作者
+    match_music = re.sub(u"\\(.*?\\)|\\{.*?}|\\[.*?]|（.*?）", "", failedMatch_list)  # 去除带括号或注释的字符,否则影响判断
+    while "" in match_authors:  # 去除列表中的空字符串
+        match_authors.remove("")
 
-        unique_matchName = [i for i in matchName if i not in matched]  # 找到
-        unique_matched = [i for i in matched if i not in matchName]
-        if len(unique_matchName) == 0 or len(unique_matched) == 0:
-            print("匹配度:", matchName, matched, matchedMusic_list)
-        else:
-            failedMatch.append(failedMatch_list)
+    # matchName = re.split(r'[\s.、()（）&:_\\/-]',  # "".join(musicName.split()).lower()
+    #                      failedMatch_list.lower())  # 一丝不挂 - 陈奕迅 分割文本 好进行匹配
+    # while "" in matchName:  # 去除列表中的空字符串
+    #     matchName.remove("")
+    for matchedMusicKey, matchedMusicValue in matchedMusic_list.items():
+        matched_music_author = re.split(r'[-]', matchedMusicValue[0].lower())
+        matched_authors = re.split(r'[\s.、()（）&:_\\/-]', matched_music_author[-1].lower())  # 分割出被匹配的作者
+        # matched = re.split(r'[\s.、()（）&:_\\/-]',  # "".join(musicName.split()).lower()
+        #                    matchedMusicValue[0].lower())
+        while "" in matched_authors:  # 去除列表中的空字符串
+            matched_authors.remove("")
+        # matchDegree = len(matchName) if len(matchName) <= len(matched) else len(matched)
+        # check_list = matchName if len(matchName) <= len(matched) else matched
+
+        # for music_author in matched_music_author:
+        #     # unique_matchName = [i for i in matchName if i not in matched]  # 找到
+        #     # unique_matched = [i for i in matched if i not in matchName]
+        #     # if len(unique_matchName) == 0 or len(unique_matched) == 0:
+        #     #     print("匹配度:", matchName, matched, matchedMusic_list)
+        #     # else:
+        #     failedMatch.append(failedMatch_list)
 
 
 def outputM3u():
@@ -128,4 +148,6 @@ if __name__ == '__main__':
     # matchedMusic_list={}
     findAndInput('./data', 'MusicPath.txt')  # './t从网易云上扒歌单,在本地配对好数据,生成本地歌单m3u文件rash_data/test_DAISHI DANCE.txt'
     failedMatch = list(dict.fromkeys(failedMatch))
-    print("数据未找
+    print("数据未找到个数:", len(failedMatch))
+    for i in failedMatch:
+        print(i)
--- trash_1.py
+++ trash_1.py
@@ -5,10 +5,58 @@
  TODO @desc: 
                 
 """
-list1 = ['a','b','c','d']
-list2 = ['a','b','e']
+import re
+import difflib
+
+list1 = ['a', 'b', 'c', 'd']
+list2 = ['a', 'b', 'e']
 
 list1_not_in_list2 = [i for i in list1 if i not in list2]
 print(list1_not_in_list2)
 list2_not_in_lis1 = [i for i in list2 if i not in list1]
 print(list2_not_in_lis1)
+str_1 = "书hi(45646)(翻自矶村由纪子）333（Piano ver）（我）4{564}《无涯》(樱散零乱)1 是 P.I.A.N.O.Reno&&《无涯》&vation.-DAISHI/DANCE/吉田兄弟 &:_\\/- 姐妹"
+str_2 = "风居住的街道(翻自矶村由纪子）333（Piano ver）-饭碗的彼岸"
+print(re.sub(u"\\(.*?\\)|\\(.*?）|\\（.*?\\)|\\{.*?}|\\[.*?]|（.*?）|《.*?》|\s|/|\.|&|\\\|:|_", "", str_1))
+#
+print(re.sub(u"\\(.*?）|", "", str_2))
+# match_music = musicName[:musicName.find('-')]  # 从左往右 只要到第一个"-" 视为歌名
+# match_author = musicName[musicName.rfind('-'):]  # 从右往左 只要到第一个"-" 视为作者
+# match_author_music = match_music + match_author  # 匹配值: 歌名-作者
+str_3 = "防弹少年团 - 春日(feat. _SS_ROU)(FRANKOWO Remix)中文改词版 - Frank_Jiang"
+str_4 = "Frank_Jiang _SS_ROU AEllse - 防弹少年团 - 春日(feat. _SS_ROU)(FRANKOWO Remix)中文改词版"
+str_3 = re.sub(u"\\(.*?\\)|\\(.*?）|\\（.*?\\)|\\{.*?}|\\[.*?]|（.*?）|《.*?》|\s|/|\.|&|\\\|:|_", "", str_3)
+str_4 = re.sub(u"\\(.*?\\)|\\(.*?）|\\（.*?\\)|\\{.*?}|\\[.*?]|（.*?）|《.*?》|\s|/|\.|&|\\\|:|_", "", str_4)
+str_3_match = re.split("-", str_3)
+str_4_matched = re.split("-", str_4)
+Degree = 0  # 匹配成功个数
+for matched in str_4_matched:
+    for match in str_3_match:
+        if difflib.SequenceMatcher(None, matched.lower(), match.lower()).quick_ratio() > 0.85:
+            Degree += 1
+print(Degree)
+# matchValue = difflib.SequenceMatcher(None, str_4.lower(),
+#                                      str_3.lower()).quick_ratio()
+# print(matchValue)
+
+"""
+TheNights-AviciiNicholasFurlong
+The Nights (UK Version) - Avicii=F:/缓存音乐/Music1/The Nights (UK Version) - Avicii.flac=22.01817035675049
+WakeMeUp-AviciiAloeBlacc
+Wake Me Up - Avicii=F:/缓存音乐/Music1/Wake Me Up - Avicii.flac=30.364601135253906
+PIANORenovation-DAISHIDANCE吉田兄弟
+
+Champion-BarnsCourtney
+Champion=F:/缓存音乐/Music1/Champion.mp3=2.906047821044922
+Centuries-FallOutBoy
+Centuries.mp3=F:/缓存音乐/Music1/Centuries.mp3=8.71359920501709
+友谊之光-玛莉亚
+友谊之光 - Maria.flac=F:/缓存音乐/Music1/友谊之光 - Maria.flac=11.279719352722168
+风居住的街道(翻自矶村由纪子）-饭碗的彼岸
+饭碗的彼岸 - 风居住的街道（Piano ver）.mp3=F:/缓存音乐/Music1/饭碗的彼岸 - 风居住的街道（Piano ver）.mp3=10.941781997680664
+防弹少年团-春日中文改词版-FrankJiang
+Frank_Jiang _SS_ROU AEllse - 防弹少年团 - 春日(feat. _SS_ROU)(FRANKOWO Remix)中文改词版.mp3=F:/缓存音乐/Music1/Frank_Jiang _SS_ROU AEllse - 防弹少年团 - 春日(feat. _SS_ROU)(FRANKOWO Remix)中文改词版.mp3=9.155595779418945
+
+刚好遇见你-风小筝
+青玉案-云の泣
+"""
